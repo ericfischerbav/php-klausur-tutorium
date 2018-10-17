98158dbba3903523e1b26fb2c982fab85e42a007
Eric Fischer <eric.fischer.bav@gmail.com>
2018-10-17 16:23:06 +0200

    Properties hinzugefügt

---
+++ properties.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_PORT", "3306");
+define("DB_NAME", "kirchberglernen_db");
+define("DB_USER", "kirchberglernen_sql");
+define("DB_PASSWORD", "abc");
+
+?>
